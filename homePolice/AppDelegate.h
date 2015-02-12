d14227ed96862760d1221bbc529cfa7b0d174166
ios001 <ios001@ios001tekiMac-mini.local>
2015-02-12 15:51:58 +0800

    Initial Commit

---
+++ homePolice/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  homePolice
+//
+//  Created by ios001 on 15/2/12.
+//  Copyright (c) 2015年 yunzhifeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
